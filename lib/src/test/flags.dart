f432b3bd2c0eae29663f7d046f60458ba153fedc
sander <sander@sanders-iMac-Pro.local>
2022-04-05 14:02:24 -0700

    more debugging

--- lib/src/test/flags.dart
+++ lib/src/test/flags.dart
@@ -1,2 +1,7 @@
 List flagsList = [
 ];
+
+// 2022-04-05 13:59:06.760080-0700 Runner[2049:32975] flutter: pressed emoji: 🫶
+// 2022-04-05 13:59:09.917115-0700 Runner[2049:32975] flutter: pressed emoji: 🫡
+// 2022-04-05 13:59:10.833684-0700 Runner[2049:32975] flutter: pressed emoji: 🥰
+// 2022-04-05 13:59:11.904026-0700 Runner[2049:32975] flutter: pressed emoji: 🫢
