c699a67df968c8056f0c3083c3ca787778626c3b
Grabot <SanderKools@gmail.com>
2025-01-24 14:56:19 +0100

    attempting to fix ios

--- ios/Classes/EmojiKeyboardFlutterPlugin.swift
+++ ios/Classes/EmojiKeyboardFlutterPlugin.swift
@@ -1,14 +1,19 @@
 import Flutter
 import UIKit
 
-public class SwiftEmojiKeyboardFlutterPlugin: NSObject, FlutterPlugin {
+public class HelloPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "emoji_keyboard_flutter", binaryMessenger: registrar.messenger())
-    let instance = SwiftEmojiKeyboardFlutterPlugin()
+    let instance = HelloPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch call.method {
+    case "getPlatformVersion":
+      result("iOS " + UIDevice.current.systemVersion)
+    default:
+      result(FlutterMethodNotImplemented)
+    }
   }
 }
--- ios/emoji_keyboard_flutter.podspec
+++ ios/emoji_keyboard_flutter.podspec
@@ -19,5 +19,11 @@ A keyboard for only emojis
 
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
-  s.swift_version = '6.0'
+  s.swift_version = '5.0'
+
+  # If your plugin requires a privacy manifest, for example if it uses any
+  # required reason APIs, update the PrivacyInfo.xcprivacy file to describe your
+  # plugin's privacy impact, and then uncomment this line. For more information,
+  # see https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
+  # s.resource_bundles = {'hello_privacy' => ['Resources/PrivacyInfo.xcprivacy']}
 end
